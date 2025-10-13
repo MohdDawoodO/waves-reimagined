d766469277afbe1225770c74c86d5b3d59880cb1
Serious_17 <serious_17O@outlook.com>
2025-10-14 00:42:21 +0530

    handled eslint errors

--- components/providers/sidebar-provider.tsx
+++ components/providers/sidebar-provider.tsx
@@ -4,27 +4,35 @@ import { SidebarInset, SidebarProvider } from "../ui/sidebar";
 import { db } from "@/server";
 import { and, eq, ne } from "drizzle-orm";
 import { playlists } from "@/server/schema";
+import { PlaylistType } from "@/types/common-types";
 
 export default async function AppSidebarProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  let userPlaylists: PlaylistType[] = [];
+  let watchLaterPlaylist: PlaylistType | undefined = undefined;
+
   const session = await auth();
-  const userPlaylists = await db.query.playlists.findMany({
-    where: and(
-      eq(playlists.userID, session?.user.id!),
-      ne(playlists.name, "Watch Later")
-    ),
-    orderBy: (playlist, { desc }) => desc(playlist.createdOn),
-    limit: 3,
-  });
-  const watchLaterPlaylist = await db.query.playlists.findFirst({
-    where: and(
-      eq(playlists.userID, session?.user.id!),
-      eq(playlists.name, "Watch Later")
-    ),
-  });
+
+  if (session) {
+    userPlaylists = await db.query.playlists.findMany({
+      where: and(
+        eq(playlists.userID, session.user.id),
+        ne(playlists.name, "Watch Later")
+      ),
+      orderBy: (playlist, { desc }) => desc(playlist.createdOn),
+      limit: 3,
+    });
+
+    watchLaterPlaylist = await db.query.playlists.findFirst({
+      where: and(
+        eq(playlists.userID, session.user.id),
+        eq(playlists.name, "Watch Later")
+      ),
+    });
+  }
 
   return (
     <SidebarProvider className="h-[100vh] overflow-hidden">
